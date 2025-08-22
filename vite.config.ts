24cc33ef4fb3d9939869e5b5109a857c7094ed04
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2025-08-22 20:08:06 +0600

    Fix: Update deployment scripts and add gh-pages dependency

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "https://mdrakibtrofder.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
